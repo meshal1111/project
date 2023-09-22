57e2b8f1779218023d64634af1802bd7c5eec332
Ahmedalhabib04 <144736424+Ahmedalhabib04@users.noreply.github.com>
2023-09-22 19:40:37 +0300

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,22 @@
+
+public class Contact {
+//Attibute
+ private String Name;
+ private String phonenumber;
+ private String email;
+ private String address;
+ private String birthday;
+ private String notes;
+ 
+ //Constructor
+ public Contact(String na,String p,String e,String a,String b,String no) {
+	 Name=na;
+	 phonenumber=p;
+	 email=e;
+	 address=a;
+	 birthday=b;
+	 notes=no;
+	}
+ 
+ 
+}
